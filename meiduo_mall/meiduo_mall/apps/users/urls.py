9828dbe64818945a35664e8fad5d82542ceef276
dongyangxian <huazhilian19@foxmail.com>
2018-09-14 22:40:40 +0800

    0.7.1.1-短信验证初始

---
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    url(r'^sms_codes/(?P<mobile>1[3-9]\d{9})/$', views.SMSCodeView.as_view()),
+]
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from django_redis import get_redis_connection
+from rest_framework.generics import GenericAPIView
+import random
 # Create your views here.
+
+# get 127.0.0.1/register/18011112222
+from rest_framework.response import Response
+
+from meiduo_mall.libs.yuntongxun.sms import CCP
+
+
+class SMSCodeView(GenericAPIView):
+
+    def get(self, request, mobile):
+        # 验证
+        conn = get_redis_connection('verify')
+        flag = conn.get('sms_code_%s' % mobile)
+        if flag:
+            return Response({"message": "error"})
+        # 生成验证码
+        sms_code = '%06d' % random.randint(0, 999999)
+        # 保存到redis
+
+        conn.setex('sms_code_%s' % mobile, 300, sms_code)
+        print(sms_code)
+        # 发送短信
+        # CCP().send_template_sms(mobile, [sms_code, '5'], 1)
+        # 返回响应
+        return Response({"message": "OK"})
