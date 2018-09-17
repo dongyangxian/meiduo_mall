0885334722acbface2927c288f7aafbc4d1b3ec9
dongyangxian <huazhilian19@foxmail.com>
2018-09-17 23:09:09 +0800

    0.7.3.2-解决登录后前端显示undefined问题

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -216,6 +216,7 @@ REST_FRAMEWORK = {
 
 JWT_AUTH = {
     'JWT_EXPIRATION_DELTA': datetime.timedelta(days=1),
+    'JWT_RESPONSE_PAYLOAD_HANDLER': 'users.utils.jwt_response_payload_handler',
 }
 
 # 让Django认证系统使用我们自定义的模型类
