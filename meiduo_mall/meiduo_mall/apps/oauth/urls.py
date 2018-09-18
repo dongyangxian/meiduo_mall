ba194cd71aad6cdac6135b3c8cc46dd7cf8448a7
dongyangxian <huazhilian19@foxmail.com>
2018-09-18 21:58:26 +0800

    0.7.4.3-QQ扫码登录：根据code获取access_token,进而获取open_id，并判断对应的绑定关系是否存在

--- meiduo_mall/meiduo_mall/apps/oauth/urls.py
+++ meiduo_mall/meiduo_mall/apps/oauth/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from oauth import views
 
 urlpatterns = [
-    url(r'^qq/authorization/$', views.QQLoginURLView.as_view())
+    url(r'^qq/authorization/$', views.QQLoginURLView.as_view()),
+    url(r'^qq/user/$', views.QQAuthUserView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -1,9 +1,55 @@
+import requests
 from QQLoginTool.QQtool import OAuthQQ
 from django.conf import settings
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.settings import api_settings
 from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
 # Create your views here.
+from oauth.models import OAuthQQUser
+from itsdangerous import TimedJSONWebSignatureSerializer as TJS
+
+class QQAuthUserView(GenericAPIView):
+    def get(self, request):
+        # 1. 获取前端传来的code
+        code = request.query_params.get('code')
+        if not code:
+            return Response({'message': '缺少code值'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        # 2. 根据code获取access_token(需要这个初始化对象)
+        oauth = OAuthQQ(client_id=settings.QQ_CLIENT_ID,
+                        client_secret=settings.QQ_CLIENT_SECRET,
+                        redirect_uri=settings.QQ_REDIRECT_URI,
+                        state=next)
+        access_token = oauth.get_access_token(code)
+        # 3. 获取open_id
+        openid = oauth.get_open_id(access_token)
+
+        # 4. 根据openid 去模型表中进行判断。
+        try:
+            qq_user = OAuthQQUser.objects.get(openid=openid)
+        except:
+            # 如果报错，就说明没有绑定，那就进行加密，然后返回
+            tjs = TJS(settings.SECRET_KEY, 300)  # 使用TJS进行加密
+            token = tjs.dumps({'openid': openid}).decode()
+            return Response({'access_token': token})  # TODO 这里的token并不是JWT验证的，而是前端接收时的对应变量名称
+
+        # 如果存在表示，已经绑定过。获取用户，并返回token值
+        user = qq_user
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+
+        return Response({
+            "username": user.username,
+            "user_id": user.id,
+            "token": token
+        })
+
 
 class QQLoginURLView(APIView):
     """
