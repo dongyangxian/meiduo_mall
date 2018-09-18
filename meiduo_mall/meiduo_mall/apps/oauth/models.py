ee5f7058ee2d7a5c12da734d8325cb5cfa6536a8
dongyangxian <huazhilian19@foxmail.com>
2018-09-18 21:22:17 +0800

    0.7.4.2-QQ扫码登录：创建用户与QQ身份的模型表

--- meiduo_mall/meiduo_mall/apps/oauth/models.py
+++ meiduo_mall/meiduo_mall/apps/oauth/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from meiduo_mall.utils.models import BaseModel
+
+
+class OAuthQQUser(BaseModel):
+    openid = models.CharField(max_length=64, verbose_name='openid', db_index=True)  # db_index添加为索引
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='用户')
+
+    class Meta:
+        db_table = 'db_oauth_qq'
+        verbose_name = 'QQ登录用户数据'
+        verbose_name_plural = verbose_name
