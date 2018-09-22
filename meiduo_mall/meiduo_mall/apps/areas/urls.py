5a4047c91b86d38f2f003e1625d6d3c48a209dbe
dongyangxian <huazhilian19@foxmail.com>
2018-09-22 17:56:05 +0800

    0.8.1.4-市与县信息查询

--- meiduo_mall/meiduo_mall/apps/areas/urls.py
+++ meiduo_mall/meiduo_mall/apps/areas/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from .views import AreaView
+from .views import AreaView, CityView
 urlpatterns = [
-    url(r'^areas/$', AreaView.as_view())
+    url(r'^areas/$', AreaView.as_view()),
+    url(r'^areas/(?P<pk>\d+)/$', CityView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -10,3 +10,11 @@ class AreaView(ListAPIView):
     serializer_class = AreasSerializer
     # 添加查询集
     queryset = Area.objects.filter(parent=None)
+
+class CityView(ListAPIView):
+    """市区信息查询"""
+    serializer_class = AreasSerializer
+    # 查询市县的信息，需要重写get_query()
+    def get_queryset(self):
+        pk = self.kwargs['pk']   # 获取传送来的省或市的id
+        return Area.objects.filter(parent=pk)
