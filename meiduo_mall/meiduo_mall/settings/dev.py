0d6eae50d5b15b1501da31489845d799be53a9f4
dongyangxian <huazhilian19@foxmail.com>
2018-09-25 20:45:42 +0800

    0.10.1.2-使用定时任务，执行静态化页面

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
 
     'ckeditor',  # 富文本编辑器
     'ckeditor_uploader',  # 富文本编辑器上传图片模块
+
+    'django_crontab',  # 定时任务
 ]
 
 MIDDLEWARE = [
@@ -287,3 +289,11 @@ CKEDITOR_UPLOAD_PATH = ''  # 上传图片保存路径，使用了FastDFS，所�
 
 # 生成的静态html文件保存目录
 GENERATED_STATIC_HTML_FILES_DIR = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'front_end_pc')
+
+# 定时任务
+CRONJOBS = [
+    # 每5分钟执行一次生成主页静态文件
+    ('*/1 * * * *', 'contents.utils.generate_static_index_html', '>> /home/python/Desktop/meiduo_mall/meiduo_mall/logs/crontab.log')
+]
+# 解决crontab中文问题
+CRONTAB_COMMAND_PREFIX = 'LANG_ALL=zh_cn.UTF-8'
