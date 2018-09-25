26531600b49b6b5c975dbbd011f274b3f7520fd1
dongyangxian <huazhilian19@foxmail.com>
2018-09-25 12:29:14 +0800

    富文本编辑器配置及相关字段添加，迁移

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('users.urls')),
     url(r'', include('areas.urls')),
-    url(r'^oauth/', include('oauth.urls'))
+    url(r'^oauth/', include('oauth.urls')),
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]
