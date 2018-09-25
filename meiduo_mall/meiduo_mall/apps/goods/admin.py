6601608fe8fdd8818771d265014ac5631d2f5cbd
dongyangxian <huazhilian19@foxmail.com>
2018-09-25 12:34:40 +0800

    0.9.2.3-admin后台站点管理修改配置

--- meiduo_mall/meiduo_mall/apps/goods/admin.py
+++ meiduo_mall/meiduo_mall/apps/goods/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from goods import models
+
+admin.site.register(models.Goods)
+
+class Goods(admin.ModelAdmin):
+    list_display = ['id', 'name']
