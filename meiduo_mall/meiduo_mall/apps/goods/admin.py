977726835855efbd7b2d38127e59ec39d3b01b83
dongyangxian <huazhilian19@foxmail.com>
2018-09-25 21:16:56 +0800

    0.10.1.3-商品列表页静态化页面异步处理

--- meiduo_mall/meiduo_mall/apps/goods/admin.py
+++ meiduo_mall/meiduo_mall/apps/goods/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
-
+from celery_tasks.static_html.tasks import generate_static_list_search_html
 # Register your models here.
 from goods import models
 
-admin.site.register(models.Goods)
-
 class Goods(admin.ModelAdmin):
     list_display = ['id', 'name']
+
+admin.site.register(models.Goods)
+admin.site.register(models.GoodsCategory, Goods)
+admin.site.register(models.GoodsChannel)
+admin.site.register(models.Brand)
+admin.site.register(models.GoodsSpecification)
+admin.site.register(models.SpecificationOption)
+admin.site.register(models.SKU)
+admin.site.register(models.SKUSpecification)
+admin.site.register(models.SKUImage)
+
+class GoodsCategoryModelAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    def save_model(self, request, obj, form, change):
+        print(obj)
+
+        print(type(obj))
+
+        obj.save()
+
+    # 调用生成静态页面方法
+    generate_static_list_search_html.delay()
--- meiduo_mall/meiduo_mall/apps/goods/utils.py
+++ meiduo_mall/meiduo_mall/apps/goods/utils.py
@@ -1,17 +1,12 @@
-
 from collections import OrderedDict
-from django.conf import settings
-from django.template import loader
-import os
-import time
 
-from goods.models import GoodsChannel
-from .models import ContentCategory
+from .models import GoodsChannel
 
 
-def generate_static_index_html():
+def get_categories():
     """
-    生成静态的主页html文件
+    获取商城商品分类菜单
+    :return 菜单字典
     """
     # 商品频道及分类菜单
     # 使用有序字典保存类别的顺序
@@ -24,7 +19,7 @@ def generate_static_index_html():
     #
     #     }
     # }
-    categories = OrderedDict()
+    categories = {}
     channels = GoodsChannel.objects.order_by('group_id', 'sequence')
     for channel in channels:
         group_id = channel.group_id  # 当前组
@@ -46,21 +41,4 @@ def generate_static_index_html():
             for cat3 in cat2.goodscategory_set.all():
                 cat2.sub_cats.append(cat3)
             categories[group_id]['sub_cats'].append(cat2)
-
-    # 广告内容
-    contents = {}
-    content_categories = ContentCategory.objects.all()
-    for cat in content_categories:
-        contents[cat.key] = cat.content_set.filter(status=True).order_by('sequence')
-
-    # 渲染模板
-    context = {
-        'categories': categories,
-        'contents': contents
-    }
-
-    template = loader.get_template('index.html')
-    html_text = template.render(context)
-    file_path = os.path.join(settings.GENERATED_STATIC_HTML_FILES_DIR, 'index.html')
-    with open(file_path, 'w', encoding='utf-8') as f:
-        f.write(html_text)
+    return categories
