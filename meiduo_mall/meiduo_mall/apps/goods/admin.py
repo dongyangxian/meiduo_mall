3bbf64afd9357d38554a492f110082f38ae2066c
dongyangxian <huazhilian19@foxmail.com>
2018-09-27 13:31:03 +0800

    0.10.2.1-产品详情页静态化创建

--- meiduo_mall/meiduo_mall/apps/goods/admin.py
+++ meiduo_mall/meiduo_mall/apps/goods/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from celery_tasks.static_html.tasks import generate_static_list_search_html
+from celery_tasks.static_html.tasks import generate_static_list_search_html, generate_static_sku_detail_html
 # Register your models here.
 from goods import models
 
 class Goods(admin.ModelAdmin):
     list_display = ['id', 'name']
+
+class SKUModelAdmin(admin.ModelAdmin):
+    """后台创建商品时，增加商品的静态详情页"""
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        generate_static_sku_detail_html.delay(obj.id)
 
 admin.site.register(models.Goods)
 admin.site.register(models.GoodsCategory, Goods)
@@ -12,7 +18,7 @@ admin.site.register(models.GoodsChannel)
 admin.site.register(models.Brand)
 admin.site.register(models.GoodsSpecification)
 admin.site.register(models.SpecificationOption)
-admin.site.register(models.SKU)
+admin.site.register(models.SKU, SKUModelAdmin)
 admin.site.register(models.SKUSpecification)
 admin.site.register(models.SKUImage)
 
