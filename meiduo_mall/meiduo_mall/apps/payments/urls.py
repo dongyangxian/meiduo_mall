2d71717239687dd8c39d2d675885d924a920514f
dongyangxian <huazhilian19@foxmail.com>
2018-10-01 11:42:24 +0800

    0.10.6.3-支付成功显示订单号

--- meiduo_mall/meiduo_mall/apps/payments/urls.py
+++ meiduo_mall/meiduo_mall/apps/payments/urls.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 from . import views
 urlpatterns = [
     url(r'^orders/(?P<order_id>\d+)/payment/$', views.PaymentView.as_view()),
+    url(r'^payment/status/$', views.PaymentStatusView.as_view()),
 ]
